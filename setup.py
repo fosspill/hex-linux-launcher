5dc1b23d475597997fccf1e8060e3ffbc20b105c
oleerik <github.eloelo@spamgourmet.com>
2018-01-14 22:13:45 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hex-linux-launcher',
+    version='0.1',
+    packages=['pyqt5'],
+    url='',
+    license='',
+    author='oleerik',
+    author_email='',
+    description=''
+)
